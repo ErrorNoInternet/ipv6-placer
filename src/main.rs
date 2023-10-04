1f88a6ce51bbb25cdedeeede576fbcd8929e4912
ErrorNoInternet <errornointernet@envs.net>
2023-10-04 22:37:31 +0800

    Add frame support

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,21 @@ struct Arguments {
 #[derive(Debug, Subcommand)]
 enum Commands {
     #[command(arg_required_else_help = true)]
+    DrawFrames {
+        #[arg(short, long)]
+        frames_path: String,
+
+        #[arg(long, default_value_t = 0, requires = "frames_path")]
+        frame_x_offset: u32,
+
+        #[arg(long, default_value_t = 0, requires = "frames_path")]
+        frame_y_offset: u32,
+
+        #[arg(short, long, default_value_t = 1000, requires = "frames_path")]
+        wait_milliseconds: u64,
+    },
+
+    #[command(arg_required_else_help = true)]
     DrawImage {
         #[arg(short, long)]
         image: String,
@@ -60,58 +75,78 @@ fn main() {
     let placer = Arc::new(Placer::new(Ipv6Addr::new(
         0x2a01, 0x4f8, 0xc012, 0xf8e6, 0, 0, 0, 0,
     )));
+    let mut pixels = Vec::new();
     match arguments.command {
+        Commands::DrawFrames {
+            frames_path,
+            frame_x_offset,
+            frame_y_offset,
+            wait_milliseconds,
+        } => {
+            let directory_contents = match std::fs::read_dir(frames_path) {
+                Ok(frame_list) => frame_list,
+                Err(error) => {
+                    println!("unable to read directory: {error}");
+                    return;
+                }
+            };
+            let mut frame_list = Vec::new();
+            for item in directory_contents {
+                if let Ok(item) = item {
+                    frame_list.push(item.path().to_str().unwrap().to_string())
+                }
+            }
+            frame_list.sort();
+            let first_frame_pixels =
+                match build_pixels_from_image(&frame_list[0], frame_x_offset, frame_y_offset) {
+                    Ok(frame_pixels) => frame_pixels,
+                    Err(error) => {
+                        println!("unable to open frame: {error:?}");
+                        return;
+                    }
+                };
+            placer.place_batch(&first_frame_pixels, true);
+
+            let mut old_frame_pixels = first_frame_pixels;
+            for frame in &frame_list[1..] {
+                let new_frame_pixels =
+                    match build_pixels_from_image(&frame, frame_x_offset, frame_y_offset) {
+                        Ok(frame_pixels) => frame_pixels,
+                        Err(error) => {
+                            println!("unable to open frame: {error:?}");
+                            return;
+                        }
+                    };
+                let different_pixels: Vec<Pixel> = new_frame_pixels
+                    .iter()
+                    .filter(|new_pixel| {
+                        old_frame_pixels.iter().any(|old_pixel| {
+                            new_pixel.x == old_pixel.x
+                                && new_pixel.y == old_pixel.y
+                                && new_pixel.r != old_pixel.r
+                                && new_pixel.g != old_pixel.g
+                                && new_pixel.b != old_pixel.b
+                        })
+                    })
+                    .map(|item| item.to_owned())
+                    .collect();
+                old_frame_pixels = new_frame_pixels;
+                placer.place_batch(&different_pixels, true);
+                std::thread::sleep(Duration::from_millis(wait_milliseconds));
+            }
+        }
         Commands::DrawImage {
             image,
             image_x_offset,
             image_y_offset,
         } => {
-            let image_pixels = match build_pixels_from_image(&image, image_x_offset, image_y_offset)
-            {
-                Ok(image_pixels) => image_pixels,
+            match build_pixels_from_image(&image, image_x_offset, image_y_offset) {
+                Ok(image_pixels) => pixels.extend(image_pixels),
                 Err(error) => {
                     println!("unable to open image file: {error:?}");
                     return;
                 }
             };
-            let active_threads = Arc::new(Mutex::new(0));
-            let mut current_batch = Vec::new();
-            for pixel in image_pixels {
-                current_batch.push(pixel);
-                if current_batch.len() >= arguments.batch_size {
-                    let placer_arc = placer.clone();
-                    let active_threads_arc = active_threads.clone();
-                    let current_batch_arc = Arc::new(current_batch.clone());
-                    while *active_threads.lock().unwrap() > arguments.threads {
-                        std::thread::sleep(Duration::from_millis(1))
-                    }
-                    std::thread::spawn(move || {
-                        send_batch(
-                            placer_arc,
-                            current_batch_arc,
-                            active_threads_arc,
-                            !arguments.no_optimize,
-                        )
-                    });
-                    *active_threads.lock().unwrap() += 1;
-                    current_batch.clear()
-                }
-            }
-            let placer_arc = placer.clone();
-            let active_threads_arc = active_threads.clone();
-            let current_batch_arc = Arc::new(current_batch.clone());
-            std::thread::spawn(move || {
-                send_batch(
-                    placer_arc,
-                    current_batch_arc,
-                    active_threads_arc,
-                    !arguments.no_optimize,
-                )
-            });
-            *active_threads.lock().unwrap() += 1;
-            while *active_threads.lock().unwrap() > 0 {
-                std::thread::sleep(Duration::from_millis(1));
-            }
         }
         Commands::DrawPixels {
             start_x,
@@ -120,7 +155,6 @@ fn main() {
             end_y,
             color,
         } => {
-            let mut pixels = Vec::new();
             for x in start_x..end_x {
                 for y in start_y..end_y {
                     let r = (color >> 16) & 0xFF;
@@ -136,35 +170,20 @@ fn main() {
                     })
                 }
             }
-            let active_threads = Arc::new(Mutex::new(0));
-            let mut current_batch = Vec::new();
-            for pixel in pixels {
-                current_batch.push(pixel);
-                if current_batch.len() >= arguments.batch_size {
-                    let placer_arc = placer.clone();
-                    let active_threads_arc = active_threads.clone();
-                    let current_batch_arc = Arc::new(current_batch.clone());
-                    while *active_threads.lock().unwrap() > arguments.threads {
-                        std::thread::sleep(Duration::from_millis(1))
-                    }
-                    std::thread::spawn(move || {
-                        send_batch(
-                            placer_arc,
-                            current_batch_arc,
-                            active_threads_arc,
-                            !arguments.no_optimize,
-                        )
-                    });
-                    *active_threads.lock().unwrap() += 1;
-                    current_batch.clear()
-                }
-            }
-            while *active_threads.lock().unwrap() > arguments.threads {
-                std::thread::sleep(Duration::from_millis(1))
-            }
+        }
+    }
+
+    let active_threads = Arc::new(Mutex::new(0));
+    let mut current_batch = Vec::new();
+    for pixel in pixels {
+        current_batch.push(pixel);
+        if current_batch.len() >= arguments.batch_size {
             let placer_arc = placer.clone();
             let active_threads_arc = active_threads.clone();
             let current_batch_arc = Arc::new(current_batch.clone());
+            while *active_threads.lock().unwrap() > arguments.threads {
+                std::thread::sleep(Duration::from_millis(1))
+            }
             std::thread::spawn(move || {
                 send_batch(
                     placer_arc,
@@ -174,10 +193,26 @@ fn main() {
                 )
             });
             *active_threads.lock().unwrap() += 1;
-            while *active_threads.lock().unwrap() > 0 {
-                std::thread::sleep(Duration::from_millis(1));
-            }
+            current_batch.clear()
         }
+    }
+    while *active_threads.lock().unwrap() > arguments.threads {
+        std::thread::sleep(Duration::from_millis(1))
+    }
+    let placer_arc = placer.clone();
+    let active_threads_arc = active_threads.clone();
+    let current_batch_arc = Arc::new(current_batch.clone());
+    std::thread::spawn(move || {
+        send_batch(
+            placer_arc,
+            current_batch_arc,
+            active_threads_arc,
+            !arguments.no_optimize,
+        )
+    });
+    *active_threads.lock().unwrap() += 1;
+    while *active_threads.lock().unwrap() > 0 {
+        std::thread::sleep(Duration::from_millis(1));
     }
 }
 
